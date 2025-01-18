3373a8b8abc960f310ea63fedd646eaaf1d4cf66
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-01-18 18:40:19 +0100

    update service

--- openia-assistant/src/services/external.service.js
+++ openia-assistant/src/services/external.service.js
@@ -1,10 +1,26 @@
-import axios from "axios";
+import { default as axios } from 'axios';
 
-export const executeExternalTask = async (taskUrl, payload) => {
+export const run = async (options) => {
     try {
-        const response = await axios.post(taskUrl, payload);
-        console.log("Task executed: ", response.data);
+        const {
+            data = {},
+            url = '/',
+            method = 'get',
+            headers = {}
+        } = options;
+        const response = await axios({
+            url,
+            data,
+            method,
+            headers: { ...headers },
+            maxRate: [
+                100 * 1024, // 100KB/s upload limit,
+                100 * 1024  // 100KB/s download limit
+            ]
+        });
+        return response.data
     } catch (error) {
         console.error("Error executing task: ", error);
+        return null;
     }
 };
