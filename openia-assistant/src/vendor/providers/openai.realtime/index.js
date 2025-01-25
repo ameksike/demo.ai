cb6acae72ceffe9958349375bb2628577d741a27
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-01-26 00:33:32 +0100

    add OpenAIRealtime provider

---
+++ openia-assistant/src/vendor/providers/openai.realtime/index.js
@@ -0,0 +1,129 @@
+import WebSocket from "ws";
+import KsCryp from 'kscryp';
+
+import audioTool from "../../../common/audio.js";
+import speackerTool from "../../../common/speacker.js";
+import { ProviderAI } from "../../../common/provider.ai.js";
+
+class OpenAIRealtime extends ProviderAI {
+
+    constructor(config) {
+        super(config);
+        this.state = {
+            connected: "connected",
+            disconnected: "disconnected",
+            responseDelta: "response.audio.delta",
+            responsePartDone: "response.content_part.done",
+            responseDone: "response.done",
+        }
+        this.status = this.state.disconnected;
+        this.defaults = {
+            buffer: audioTool.gather()
+        };
+    }
+
+    send(base64AudioData) {
+        try {
+            this.ws.send(JSON.stringify({
+                type: "conversation.item.create",
+                item: {
+                    type: 'message',
+                    role: 'user',
+                    content: [
+                        {
+                            type: 'input_audio',
+                            audio: base64AudioData
+                        }
+                    ]
+                }
+            }));
+            this.ws.send(KsCryp.encode({ type: 'response.create' }, "json"));
+            this.logger?.log({ src: "Provider:OpenAIRealtime:Send", data: { content: base64AudioData?.length } });
+        }
+        catch (error) {
+            this.logger?.log({ src: "Provider:OpenAIRealtime:Send", error, data: { content: base64AudioData?.length } });
+        }
+    }
+
+    onIncoming(message) {
+        try {
+            const response = KsCryp.decode(message.toString(), "json");
+            switch (response.type) {
+                case this.state.responseDelta:
+                    let chunk = audioTool.getChunk(response.delta);
+                    audioTool.gather({ chunk, buffer: this.defaults.buffer });
+                    speackerTool.talk(chunk);
+                    break;
+
+                case this.state.responseDone:
+                    speackerTool.stop();
+                    break;
+
+                case this.state.responsePartDone:
+                    const { part } = response;
+                    console.log(part.transcript);
+                    console.log("\n");
+                    break;
+            }
+        }
+        catch (error) {
+            this.logger?.error({ src: "Provider:OpenAIRealtime:send", error: error.toString(), data: message });
+        }
+    }
+
+    async ask(message = "input-01", profile) {
+        const tmp = await audioTool.load(message, "db/" + profile.name + "/audio/")
+        const base64AudioData = await audioTool.toBase64(tmp)
+        this.send(base64AudioData)
+    }
+
+    async run(message, profile) {
+        try {
+            await this.connect(profile);
+            this.ask(message, profile);
+            return "Let me think about it";
+        }
+        catch (error) {
+            this.logger?.error({ src: "Provider:OpenAIRealtime:Run", data: { messages, profile } });
+        }
+    }
+
+    connect(profile) {
+        return new Promise((resolve, reject) => {
+            if (this.status === this.state.connected) {
+                return resolve(this);
+            }
+
+            let url = profile.url || "wss://api.openai.com/v1/realtime";
+            url += "?model=" + profile.model;
+
+            this.ws = new WebSocket(url, {
+                headers: {
+                    "Authorization": "Bearer " + process.env.OPENAI_API_KEY,
+                    "OpenAI-Beta": "realtime=v1",
+                    // "OpenAI-Project": process.env.PROJECT
+                },
+            });
+
+            this.ws.onerror = (error) => {
+                this.logger?.log({ src: "Provider:OpenAIRealtime:onError", error });
+                reject(error);
+            };
+
+            this.ws.on("open", () => {
+                this.status = this.state.connected;
+                this.logger?.log({ src: "Provider:OpenAIRealtime:Connected" });
+                resolve(this);
+            });
+
+            this.ws.on("message", (message) => this.onIncoming(message));
+
+        });
+    }
+
+    disconnect() {
+        this.status = this.state.disconnected;
+    }
+}
+
+export default OpenAIRealtime;
