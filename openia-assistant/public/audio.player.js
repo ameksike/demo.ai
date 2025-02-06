78beead9b6d4e113e3fdafa69785da9deb72ec7d
Antonio Membrides Espinosa <tonykssa@gmail.com>
2025-02-06 21:57:25 +0100

    improve player

--- openia-assistant/public/audio.player.js
+++ openia-assistant/public/audio.player.js
@@ -1,45 +1,95 @@
 export class StreamingAudioPlayer {
-    constructor() {
-        this.audioContext = new AudioContext();
-        this.audioQueue = [];
+    constructor(options) {
+        this.script = {
+            key: "audio-stream-processor",
+            src: "/public/audio.processor.js"
+        }
+        this.context = null;
+        this.analyser = null;
         this.isPlaying = false;
-        this.initAudioWorklet();
+        this.sampleRate = options?.sampleRate || 44100;
+        this.audioQueue = [];
+        this.init();
     }
 
-    async initAudioWorklet() {
+    /**
+     * Connects the audio context and enables output to speakers
+     * @returns {Promise<true>}
+     */
+    async connect(options) {
         try {
-            await this.audioContext.audioWorklet.addModule("/public/audio.processor.js");
-            this.audioNode = new AudioWorkletNode(this.audioContext, "audio-stream-processor");
-            this.audioNode.connect(this.audioContext.destination);
-            console.log("StreamingAudioPlayer:initAudioWorklet", this.audioContext.destination);
+            if (this.context) {
+                return this.context;
+            }
+
+            const stream = options?.stream || await navigator.mediaDevices.getUserMedia({ audio: true });
+            this.context = new AudioContext({ sampleRate: this.sampleRate });
+            if (this.context.state === 'suspended') {
+                await this.context.resume();
+            }
+            this.analyser = this.context.createAnalyser();
+            this.analyser.fftSize = 8192;
+            this.analyser.smoothingTimeConstant = 0.1;
+            return true;
+        }
+        catch (err) {
+            console.error("StreamingAudioPlayer:connect", err);
+            this.context = null;
+        }
+    }
+
+    /**
+     * Starts audio streaming
+     * @private
+     * @returns {Promise<AudioWorkletNode|null>}
+     */
+    async init() {
+        try {
+            if (this.audioNode && this.context) {
+                return this.audioNode;
+            }
+            await this.connect();
+            await this.context.audioWorklet.addModule(this.script.src);
+            this.audioNode = new AudioWorkletNode(this.context, this.script.key);
+            this.audioNode.connect(this.context.destination);
+
+            console.log("StreamingAudioPlayer:init", this.context.destination);
+
+            return this.audioNode;
         }
         catch (error) {
-            console.log("StreamingAudioPlayer:initAudioWorklet", error);
+            console.error("StreamingAudioPlayer:init", error);
+            return null;
         }
     }
 
-    async play(audio) {
+    /**
+     * Adds 16BitPCM data to the currently playing audio stream
+     * You can add chunks beyond the current play point and they will be queued for play
+     * @param {ArrayBuffer|Int16Array} arrayBuffer
+     * @param {string} [trackId]
+     * @returns {Int16Array}
+     */
+    async play(audio, trackId = 'default') {
+
+        console.log("StreamingAudioPlayer:play", { audio, trackId });
         const audioBuffer = typeof audio === "string" ? await this.decodeAudio(audio) : audio;
         const rawData = audioBuffer.getChannelData ? audioBuffer.getChannelData(0) : audioBuffer;
-        if (!this.audioNode) {
-            await this.initAudioWorklet();
+        await this.init();
+
+        let buffer;
+        if (rawData instanceof Int16Array) {
+            buffer = rawData;
+        } else if (rawData instanceof ArrayBuffer) {
+            buffer = new Int16Array(rawData);
+        } else {
+            throw new Error(`argument must be Int16Array or ArrayBuffer`);
         }
-        this.audioNode.port.postMessage(rawData);
+        this.audioNode.port.postMessage({ event: 'write', buffer, trackId });
     }
 
     async decodeAudio(base64Audio) {
         const arrayBuffer = await fetch(`data:audio/wav;base64,${base64Audio}`).then(res => res.arrayBuffer());
-        return this.audioContext.decodeAudioData(arrayBuffer);
+        return this.context.decodeAudioData(arrayBuffer);
     }
 }
-
-/**
-// Uso en WebSocket
-const player = new StreamingAudioPlayer();
-const ws = new WebSocket("ws://localhost:3000");
-
-ws.onmessage = (event) => {
-  const data = JSON.parse(event.data);
-  if (data.audio) player.play(data.audio);
-};
- */
--- openia-assistant/public/audio.processor.js
+++ openia-assistant/public/audio.processor.js
@@ -1,16 +1,89 @@
 class AudioStreamProcessor extends AudioWorkletProcessor {
     constructor() {
+        console.log("AudioStreamProcessor..........")
         super();
-        this.buffer = [];
-        this.port.onmessage = (event) => this.buffer.push(event.data);
+        this.hasStarted = false;
+        this.hasInterrupted = false;
+        this.outputBuffers = [];
+        this.bufferLength = 128;
+        this.write = { buffer: new Float32Array(this.bufferLength), trackId: null };
+        this.writeOffset = 0;
+        this.trackSampleOffsets = {};
+        this.port.onmessage = (event) => {
+            if (event.data) {
+                const payload = event.data;
+                if (payload.event === 'write') {
+                    const int16Array = payload.buffer;
+                    const float32Array = new Float32Array(int16Array.length);
+                    for (let i = 0; i < int16Array.length; i++) {
+                        float32Array[i] = int16Array[i] / 0x8000; // Convert Int16 to Float32
+                    }
+                    this.writeData(float32Array, payload.trackId);
+                } else if (
+                    payload.event === 'offset' ||
+                    payload.event === 'interrupt'
+                ) {
+                    const requestId = payload.requestId;
+                    const trackId = this.write.trackId;
+                    const offset = this.trackSampleOffsets[trackId] || 0;
+                    this.port.postMessage({
+                        event: 'offset',
+                        requestId,
+                        trackId,
+                        offset,
+                    });
+                    if (payload.event === 'interrupt') {
+                        this.hasInterrupted = true;
+                    }
+                } else {
+                    throw new Error(`Unhandled event "${payload.event}"`);
+                }
+            }
+        };
     }
 
-    process(inputs, outputs) {
-        console.log("AudioStreamProcessor", { inputs, outputs });
-        if (this.buffer.length > 0) {
-            let output = outputs[0];
-            output[0].set(this.buffer.shift());
+    process(inputs, outputs, parameters) {
+        console.log("AudioStreamProcessor", { inputs, outputs, parameters });
+        const output = outputs[0];
+        const outputChannelData = output[0];
+        const outputBuffers = this.outputBuffers;
+        if (this.hasInterrupted) {
+            this.port.postMessage({ event: 'stop' });
+            return false;
+        } else if (outputBuffers.length) {
+            this.hasStarted = true;
+            const { buffer, trackId } = outputBuffers.shift();
+            for (let i = 0; i < outputChannelData.length; i++) {
+                outputChannelData[i] = buffer[i] || 0;
+            }
+            if (trackId) {
+                this.trackSampleOffsets[trackId] =
+                    this.trackSampleOffsets[trackId] || 0;
+                this.trackSampleOffsets[trackId] += buffer.length;
+            }
+            return true;
+        } else if (this.hasStarted) {
+            this.port.postMessage({ event: 'stop' });
+            return false;
+        } else {
+            return true;
         }
+    }
+
+
+    writeData(float32Array, trackId = null) {
+        let { buffer } = this.write;
+        let offset = this.writeOffset;
+        for (let i = 0; i < float32Array.length; i++) {
+            buffer[offset++] = float32Array[i];
+            if (offset >= buffer.length) {
+                this.outputBuffers.push(this.write);
+                this.write = { buffer: new Float32Array(this.bufferLength), trackId };
+                buffer = this.write.buffer;
+                offset = 0;
+            }
+        }
+        this.writeOffset = offset;
         return true;
     }
 }
